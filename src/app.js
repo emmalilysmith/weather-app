6d82ba8ea26333fffe4d5fa7a308e0009904185c
emmalilysmith <96999741+emmalilysmith@users.noreply.github.com>
2022-01-09 16:57:53 -0500

    added relevant icon from api

--- src/app.js
+++ src/app.js
@@ -28,16 +28,24 @@ function displayTemperature(response) {
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#windSpeed");
   let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "5b3b9effb6902fde550fd54c7766fe1b";
+let city = "toronto";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?
-q=Toronto&appid=${apiKey}&units=metric`;
+q=${city}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
