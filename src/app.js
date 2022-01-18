2d1cc11f2bac16e8024d5ed9b5880a2f10d269db
emmalilysmith <96999741+emmalilysmith@users.noreply.github.com>
2022-01-17 19:36:26 -0500

    added the search engine

--- src/app.js
+++ src/app.js
@@ -43,9 +43,21 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-let apiKey = "5b3b9effb6902fde550fd54c7766fe1b";
-let city = "toronto";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?
+function search(city) {
+  let apiKey = "5b3b9effb6902fde550fd54c7766fe1b";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?
 q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
+  console.log(cityInputElement.value);
+}
+
+search("Toronto");
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
